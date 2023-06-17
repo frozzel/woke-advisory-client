17e5276b5ee40f4375c3f749993ad42d0aae575d
Dennis Hickox <frozzel@me.com>
2023-06-17 13:39:27 -0400

    test4

--- src/api/client.js
+++ src/api/client.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const client = axios.create({baseURL: "https://woke-advisory-server-yyyxt.ondigitalocean.app"});
+const client = axios.create({baseURL: "https://woke-advisory-server-yyyxt.ondigitalocean.app/api"});
 
 export default client;
