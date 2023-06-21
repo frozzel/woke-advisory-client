c6fca0e8dba44c0e548c8d0b6cc67c0f06c3ef2f
Dennis Hickox <frozzel@me.com>
2023-06-21 13:29:51 -0400

    movie reviews span added

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const client = axios.create({baseURL: "https://api.wokeadvisory.com/api"});
+const client = axios.create({baseURL: "http://localhost:8080/api"});
 
 export default client;
 
