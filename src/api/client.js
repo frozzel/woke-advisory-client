a16b0a6f8f429c5434e6334215e94904ac11fa28
Dennis Hickox <frozzel@me.com>
2023-06-23 12:08:14 -0400

    test api

--- src/api/client.js
+++ src/api/client.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const client = axios.create({baseURL: process.env.API || "http://localhost:8080/api"});
+const client = axios.create({baseURL: process.env.API});
 
 export default client;
