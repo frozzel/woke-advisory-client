ce3ecbc475ff59a0d1ef49fab34e6c6f30b12a24
Dennis Hickox <frozzel@me.com>
2023-06-30 14:12:20 -0400

    uiSearch changes updated global search

--- src/components/SchoolHome.jsx
+++ src/components/SchoolHome.jsx
@@ -1,16 +1,44 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Container from './Container'
 import NotVerified from './user/NotVerified'
 import AppSearchForm from './form/AppSearchForm'
-import { useNavigate } from 'react-router-dom'
 import HeroSlideshowNews from './user/HeroSlideshowNews'
+import { searchPublicSchools} from "../api/school";
+import { useNotification } from "../hooks";
+import NotFoundText from "./NotFoundText";
+import SchoolList from "./user/SchoolList";
+// import TopRatedMovies from "./user/TopRatedMovies";
+
 
 export default function SchoolHome() {
-    const navigate = useNavigate();
+    const [query, setQuery] = useState("hope");
+    const [schools, setSchools] = useState([]);
+    const [resultNotFound, setResultNotFound] = useState(false);
+
+    const { updateNotification } = useNotification();
 
     const handleSearchSubmit = (query) => {
-        navigate("/school/search?SchoolName=" + query);
+        setQuery(query);
       };
+
+    const searchSchool = async (query) => {
+      const { error, results } = await searchPublicSchools(query);
+     
+      if (error) return updateNotification("error", error);
+      if (!results.length) {
+        setResultNotFound(true);
+  
+        return setSchools([]);
+      }
+    
+      setResultNotFound(false);
+      setSchools([...results]);
+    };
+
+    useEffect(() => {
+      if (query.trim()) searchSchool(query);
+
+      }, [query]);
     
   return (
     <div className="dark:bg-primary bg-white min-h-screen">
@@ -18,17 +46,36 @@ export default function SchoolHome() {
                         <div className="dark:text-white text-primary text-lg pt-2 "></div>
         <HeroSlideshowNews />
         <NotVerified />
-        <AppSearchForm placeholder='Search Schools' inputClassName="border-2 border-dark-subtle p-1 rounded bg-transparent text-sm outline-none focus:border-white transition text-white sm:w-auto w-40 sm:text-lg "
+        <AppSearchForm placeholder='Search Schools' inputClassName="border-2 border-light-subtle dark:border-dark-subtle  p-1 rounded bg-transparent text-sm outline-none focus:border-secondary focus:dark:border-white transition text-light-subtle dark:text-white sm:w-auto w-40 sm:text-lg "
               onSubmit={handleSearchSubmit} />
-
-        {/* Most rated movies */}
+      
         <div className="space-y-3 py-8">
-          {/* <TopRatedMovies />
-          
-          <TopRatedTVSeries />
-          <TopRatedWebSeries /> */}
+        <SearchSchools 
+          query={query}
+          resultNotFound={resultNotFound}
+          schools={schools}/>
+        
+        
+        </div>
+        <div>
+        {/* <TopRatedMovies /> */}
         </div>
         </Container>
     </div>
   )
+}
+const SearchSchools= ({query, schools, resultNotFound}) =>{
+return (<>
+  <div className="dark:bg-primary bg-white  py-8">
+  <Container className="px-2 xl:p-0">
+     <h1 className="text-2xl dark:text-white text-secondary font-semibold mb-4">
+     Search Results:  <span className="text-primary dark:text-dark-subtle"> <span></span> "{query}<span>"</span></span></h1>
+    <NotFoundText text="Record not found!" visible={resultNotFound} />
+    <SchoolList schools={schools}  />
+  </Container>
+  </div>
+
+
+</>
+)
 }
