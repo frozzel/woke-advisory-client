c9cfdc50b901b65db52ae1bee65417b8a2348c09
Dennis Hickox <frozzel@me.com>
2023-07-01 14:17:12 -0400

    minor ui fix for safari browser

--- src/components/SchoolHome.jsx
+++ src/components/SchoolHome.jsx
@@ -7,7 +7,7 @@ import { searchPublicSchools} from "../api/school";
 import { useNotification } from "../hooks";
 import NotFoundText from "./NotFoundText";
 import SchoolList from "./user/SchoolList";
-// import TopRatedMovies from "./user/TopRatedMovies";
+import {IoSchool} from "react-icons/io5";
 
 
 export default function SchoolHome() {
@@ -43,13 +43,21 @@ export default function SchoolHome() {
   return (
     <div className="dark:bg-primary bg-white min-h-screen">
     <Container className="px-2 xl:p-0">
-                        <div className="dark:text-white text-primary text-lg pt-2 "></div>
+                        <div className="dark:text-white text-primary text-lg pt-2 outline-none"></div>
         <HeroSlideshowNews />
         <NotVerified />
-        <AppSearchForm placeholder='Search Schools' inputClassName="border-2 border-light-subtle dark:border-dark-subtle  p-1 rounded bg-transparent text-sm outline-none focus:border-secondary focus:dark:border-white transition text-light-subtle dark:text-white sm:w-auto w-40 sm:text-lg "
+        <div className="space-y-3 pt-3">
+          <p className="text-primary dark:text-dark-subtle">Get Involved! Review your schools and teachers, send out alerts to
+          inform and connect with other parents. Stop the woke indoctrination of our children by taking back your schools, movies and companies one movement at a time! This starts at the local
+          level by simply growing a network of like minded parents to build campaigns to remove teachers, administrators and board members that
+          are forcing there belief system and endangering our children.</p>
+          <div className="dark:text-highlight-dark text-highlight text-lg pt-2 outline-none text-center md:text-left">
+          <IoSchool className="inline-block w-6 h-6 mr-2 mb-2" /> Search Schools
+        <AppSearchForm placeholder='example "Hope"' inputClassName="border-2 border-light-subtle dark:border-dark-subtle  p-1 rounded bg-transparent text-sm outline-none focus:border-secondary focus:dark:border-white transition text-light-subtle dark:text-white sm:w-auto w-80 sm:text-lg"
               onSubmit={handleSearchSubmit} />
-      
-        <div className="space-y-3 py-8">
+              </div>
+        </div>
+        <div className="space-y-3 py-1">
         <SearchSchools 
           query={query}
           resultNotFound={resultNotFound}
@@ -69,7 +77,7 @@ return (<>
   <div className="dark:bg-primary bg-white  py-8">
   <Container className="px-2 xl:p-0">
      <h1 className="text-2xl dark:text-white text-secondary font-semibold mb-4">
-     Search Results:  <span className="text-primary dark:text-dark-subtle"> <span></span> "{query}<span>"</span></span></h1>
+     Search Results:  <span className="text-light-subtle dark:text-dark-subtle"> <span></span> "{query}<span>"</span></span></h1>
     <NotFoundText text="Record not found!" visible={resultNotFound} />
     <SchoolList schools={schools}  />
   </Container>
