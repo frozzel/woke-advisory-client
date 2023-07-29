46e1b4b2274764d1ec6918fb85ac49ecc3aaae95
Dennis Hickox <frozzel@me.com>
2023-07-29 13:54:11 -0400

    fix env

--- src/components/HomeTest.jsx
+++ src/components/HomeTest.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:8080');
+const socket = io(process.env.REACT_APP_API2);
 
 function HomeTest() {
   const [name, setName] = useState('');
