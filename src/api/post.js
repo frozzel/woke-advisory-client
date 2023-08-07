752d6e7050eb4b668ca38f1244bf80f3a1dbc93a
Dennis Hickox <frozzel@me.com>
2023-08-07 15:40:38 -0400

    feed woking across users

--- src/api/post.js
+++ src/api/post.js
@@ -1,10 +1,11 @@
 import { catchError, getToken } from "../utils/helper";
 import client from "./client";
 
-export const addAlertsSchool = async (schoolId, reviewData, ) => {
+export const addAlertsSchool = async (userId, reviewData, ) => {
     const token = getToken();
+    
     try {
-      const { data } = await client.post(`/alertsschool/add/${schoolId}`, reviewData, {
+      const { data } = await client.post(`/post/add/${userId}`, reviewData, {
         headers: {
           authorization: "Bearer " + token,
         },
@@ -14,45 +15,45 @@ export const addAlertsSchool = async (schoolId, reviewData, ) => {
       return catchError(error);
     }
   };
-  export const getAlertsSchool = async (userId, userData) => {
-    const token = getToken();
-    try {
-      const { data } = await client(`/post/${userId}`, userData,{
-        headers: {
-            authorization: "Bearer " + token,
-            },
-      });
-      return data;
-    } catch (error) {
-      return catchError(error);
-    }
-  };
-  export const deleteReview = async (alertId) => {
-    const token = getToken();
-    try {
-      const { data } = await client.delete(`/alertsschool/delete/${alertId}`, {
-        headers: {
+export const getAlertsSchool = async (userId, userData) => {
+  const token = getToken();
+  try {
+    const { data } = await client(`/post/${userId}`, userData,{
+      headers: {
           authorization: "Bearer " + token,
-        },
-      });
-      return data;
-    } catch (error) {
-      return catchError(error);
-    }
-  };
-  export const updateReviewSchool = async (reviewId, reviewData) => {
-    const token = getToken();
-    try {
-      const { data } = await client.patch(`/reviewschool/${reviewId}`, reviewData, {
-        headers: {
-          authorization: "Bearer " + token,
-        },
-      });
-      return data;
-    } catch (error) {
-      return catchError(error);
-    }
-  };
+          },
+    });
+    return data;
+  } catch (error) {
+    return catchError(error);
+  }
+};
+export const deleteReview = async (alertId) => {
+  const token = getToken();
+  try {
+    const { data } = await client.delete(`/post/delete/${alertId}`, {
+      headers: {
+        authorization: "Bearer " + token,
+      },
+    });
+    return data;
+  } catch (error) {
+    return catchError(error);
+  }
+};
+export const updateReviewSchool = async (reviewId, reviewData) => {
+  const token = getToken();
+  try {
+    const { data } = await client.patch(`/reviewschool/${reviewId}`, reviewData, {
+      headers: {
+        authorization: "Bearer " + token,
+      },
+    });
+    return data;
+  } catch (error) {
+    return catchError(error);
+  }
+};
 export const getReviewByUser = async (userId) => {
   try {
     const { data } = await client(`/reviewschool/get-reviews-by-user/${userId}`);
@@ -65,7 +66,7 @@ export const addComment = async (alertId, query) => {
 
   const token = getToken();
   try {
-    const { data } = await client.post(`/alertsschool/addcomment/${alertId}`, query, {
+    const { data } = await client.post(`/post/addcomment/${alertId}`, query, {
       headers: {
         authorization: "Bearer " + token,
       },
@@ -80,7 +81,7 @@ export const likeAlert = async (alertId) => {
   
     const token = getToken();
     try {
-      const { data } = await client.post(`/alertsschool/addlike/${alertId}`, {}, {
+      const { data } = await client.post(`/post/addlike/${alertId}`, {}, {
         headers: {
           authorization: "Bearer " + token,
         },
